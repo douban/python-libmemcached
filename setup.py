ef30348ddaf7cd1f283d6ae7a008018f27ba1c71
Davies Liu <davies.liu@gmail.com>
2012-06-02 23:51:04 +0800

    use Cython to build

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 from setuptools import setup, Extension
+from Cython.Distutils import build_ext
 
 setup(
         name = "python-libmemcached",
-        version = "0.40",
+        version = "1.0",
         description="python memcached client wrapped on libmemcached",
         maintainer="davies",
         maintainer_email="davies.liu@gmail.com",
-        install_requires = ['pyrex'],
+        install_requires = ['cython'],
+        cmdclass = {'build_ext': build_ext},
         ext_modules=[Extension('cmemcached_imp', 
             ['cmemcached_imp.pyx', 'split_mc.c'],
             libraries=['memcached'],
