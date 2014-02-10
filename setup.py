42dfae51552dc53a90303de2510cf69086cd2e83
xutao <xutao@douban.com>
2014-02-10 12:08:02 +0800

    Use setuptools_cython.

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@
 
 import os
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
+
+# setuptools DWIM monkey-patch madness
+# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
+import sys
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
 
 base_dir = '/Users/CMGS/Documents/Workplace/sources/test/greenify-test/'
 
@@ -12,20 +18,17 @@ if os.environ.get('LIBRARY_DIRS'):
 if os.environ.get('INCLUDE_DIRS'):
     options['include_dirs'] = [os.environ['INCLUDE_DIRS']]
 
-setup(
-        name = "python-libmemcached",
-        version = "1.0",
-        description="python memcached client wrapped on libmemcached",
-        maintainer="davies",
-        maintainer_email="davies.liu@gmail.com",
-        install_requires = ['cython>=0.18'],
-        cmdclass = {'build_ext': build_ext},
-        ext_modules=[Extension('cmemcached_imp',
-            ['cmemcached_imp.pyx', 'split_mc.c'],
-            libraries=['memcached'],
-            **options)],
-
-        py_modules=['cmemcached'],
-        tests_require = ['nose', 'mock'],
-        test_suite="nose.collector",
-)
+setup(name="python-libmemcached",
+      version="1.0",
+      description="python memcached client wrapped on libmemcached",
+      maintainer="davies",
+      maintainer_email="davies.liu@gmail.com",
+      setup_requires=['setuptools_cython'],
+      install_requires=['cython>=0.18'],
+      ext_modules=[Extension('cmemcached_imp',
+                             ['cmemcached_imp.pyx', 'split_mc.c'],
+                             libraries=['memcached'],
+                             **options)],
+      py_modules=['cmemcached'],
+      tests_require=['nose', 'mock'],
+      test_suite="nose.collector",)
