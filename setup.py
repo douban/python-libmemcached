a5ad987d3466cbd8a94ae835918165279a5c607b
Qiangning Hong <hongqn@douban.com>
2014-08-14 12:17:29 +0800

    support ci

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from setuptools import setup, Extension
+from setuptools.command.test import test as TestCommand
 
 # setuptools DWIM monkey-patch madness
 # http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-import sys
 if 'setuptools.extension' in sys.modules:
     m = sys.modules['setuptools.extension']
     m.Extension.__dict__ = m._Extension.__dict__
@@ -16,11 +17,32 @@ if os.environ.get('LIBRARY_DIRS'):
 if os.environ.get('INCLUDE_DIRS'):
     options['include_dirs'] = [os.environ['INCLUDE_DIRS']]
 
+
+# support python setup.py test
+# http://pytest.org/latest/goodpractises.html#integration-with-setuptools-test-commands
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args or [])
+        sys.exit(errno)
+
 setup(name="python-libmemcached",
       version="1.0",
       description="python memcached client wrapped on libmemcached",
-      maintainer="davies",
-      maintainer_email="davies.liu@gmail.com",
+      maintainer="Qiangning Hong",
+      maintainer_email="hongqn@douban.com",
       setup_requires=['setuptools_cython'],
       install_requires=['cython>=0.18'],
       ext_modules=[Extension('cmemcached_imp',
@@ -28,5 +50,6 @@ setup(name="python-libmemcached",
                              libraries=['memcached'],
                              **options)],
       py_modules=['cmemcached'],
-      tests_require=['nose', 'mock'],
-      test_suite="nose.collector",)
+      cmdclass={'test': PyTest},
+      tests_require=['pytest', 'mock'],
+      )
